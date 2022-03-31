e1d23d1bc48d96c801363c9df8ad03b5441f0634
The Jia Laboratory @ Georgia Tech <46298520+ShuJiaLab@users.noreply.github.com>
2022-03-31 12:19:59 -0400

    Delete SimPSF directory

--- SimPSF/I00_Plane_Figure1.m
+++
@@ -1,9 +0,0 @@
-function I00_Plane_Figure1(Diameter,step,Axial_coordinate,strf)
-%
-
-%%
-        Img_y = linspace(-Diameter/2,     +Diameter/2,... 
-                  length(-Diameter/2:step:+Diameter/2));
-        Img_x = ones(length(Img_y)).*Axial_coordinate;
-        plot(Img_x, Img_y,'b');
-        text(Axial_coordinate,-Diameter*0.6,strf,'FontSize',20);
